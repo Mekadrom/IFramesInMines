a5208ad6d2530724db133351fb5250e7de885a63
Mekadrom <zadarimm@gmail.com>
2023-04-25 00:59:35 -0400

    Initial commit

---
+++ IFramesInMines.cs
@@ -0,0 +1,22 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace IFramesInMines
+{
+    internal sealed class IFramesInMinesMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Player.Warped += this.OnWarped;
+        }
+
+        private void OnWarped(object sender, WarpedEventArgs e)
+        {
+            if (e.NewLocation != e.OldLocation && e.NewLocation.NameOrUniqueName.ToLower().StartsWith("undergroundmine"))
+            {
+                e.Player.currentTemporaryInvincibilityDuration = 1500;
+                e.Player.temporarilyInvincible = true;
+            }
+        }
+    }
+}
